34959121bbe5b0b849360daea5ff2e51654d135c
ioana <rusuioana37@gmail.com>
2025-05-06 21:39:22 +0300

    created workspace

--- front-aware/src/main.tsx
+++ front-aware/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TestTailwind from './TestTailwind';
+import TestTailwind from './test';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
